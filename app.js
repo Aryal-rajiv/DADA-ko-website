f6799389a9887ba293c824bd3196ab106a0a1588
Zitihskx <33897365+Zitihskx@users.noreply.github.com>
2021-12-06 08:10:57 +0545

    On progress

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ menu.addEventListener('click', function)
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle
 }
+function addText(){
+    document.getElementById("Research__card1").innerText= "This will direct you to my research on Machine Learning";
+}
+function removeText(){
+    document.getElementById("Research__Card1").innerText= "";
+}
+
+//Scroll to top button
+
+function scrollToTop() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
